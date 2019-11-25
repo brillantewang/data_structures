d68572c82e2d2f2b05bce4a79c06dece1f84b2af
Brillante Wang <brillante.wang@gmail.com>
2019-11-25 01:20:49 -0600

    fix remove() by tracking parent, add replaceNode(), and print node instead of data for traversal

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -33,22 +33,35 @@ class BinarySearchTreeV1 {
     }
   }
 
-  remove(data, node = this.root) {
-    if (node === null) return;
-    if (data < node.data) this.remove(data, node.left);
-    if (data > node.data) this.remove(data, node.right);
-    if (data === node.data) {
-      if (node.left && node.right) {
-        const minRightNode = getMinNode(node.right);
+  remove(data, node = this.root, parent = null) {
+    if (node === null) {
+      console.log('node not found');
+    } else if (data < node.data) {
+      this.remove(data, node.left, node);
+    } else if (data > node.data) {
+      this.remove(data, node.right, node);
+    } else if (data === node.data) {
+      if (node.left && node.right) { // has two children
+        const minRightNode = this.getMinNode(node.right);
         node.data = minRightNode.data;
-        minRightNode = null;
-      } else if (node.left) {
-        node = node.left;
-      } else if (node.right) {
-        node = node.right
-      } else {
-        node = null;
+        this.remove(minRightNode.data, node.right, node);
+      } else if (node.left) { // has one child (left)
+        this.replaceNode(node, parent, node.left);
+      } else if (node.right) { // has one child (right)
+        this.replaceNode(node, parent, node.right);
+      } else { // has no children
+        this.replaceNode(node, parent, null);
       }
+    }
+  }
+
+  replaceNode(node, parent, replacement) {
+    if (parent === null) { // this means the node is the root
+      this.root = replacement;
+    } else if (node.data < parent.data) { // this means the node is the left child
+      parent.left = replacement;
+    } else { // this means the node is the right child
+      parent.right = replacement;
     }
   }
 
@@ -67,7 +80,7 @@ class BinarySearchTreeV1 {
   inOrderTraversal(node = this.root) {
     if (this.root === null) return;
     if (node.left) this.inOrderTraversal(node.left);
-    console.log(node.data);
+    console.log(node);
     if (node.right) this.inOrderTraversal(node.right);
   }
 }
@@ -78,6 +91,16 @@ bst.add(7);
 bst.add(12);
 bst.add(6);
 bst.add(2);
-bst.inOrderTraversal(); // 2, 5, 6, 7, 12
-console.log(bst.contains(3)) // false
-console.log(bst.contains(7)) // true
+bst.add(3);
+// bst.inOrderTraversal(); // 2, 3, 5, 6, 7, 12
+// console.log(bst.getMinNode());
+// console.log(bst.contains(8)) // false
+// console.log(bst.contains(7)) // true
+// bst.remove(3);
+// bst.inOrderTraversal(); // 2, 5, 6, 7, 12
+// bst.remove(2);
+// bst.inOrderTraversal(); // 3, 5, 6, 7, 12
+// bst.remove(7);
+// bst.inOrderTraversal(); // 2, 3, 5, 6, 12
+bst.remove(5);
+bst.inOrderTraversal(); // 2, 3, 6, 7, 12
