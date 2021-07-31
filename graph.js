4dbf3e2bc11f73da21a866288ca4f398575a71b6
Brillante Wang <brillante.wang@gmail.com>
2021-07-31 16:08:58 -0500

    Add hasPathBfs
    also inspired by https://youtu.be/zaBhtODEL0w

--- graph.js
+++ graph.js
@@ -39,7 +39,25 @@ class Graph {
     };
 
     return false;
-  }
+  };
+
+  hasPathBfs(node1, node2) {
+    let visitedNodes = new Set();
+    let queue = [node1];
+
+    while (queue.length > 0) {
+      const node = queue.shift();
+      if (node === node2) return true;
+
+      if (visitedNodes.has(node)) continue;
+      visitedNodes.add(node);
+
+      const adjacencies = this.adjacencyList[node];
+      adjacencies.forEach(node => queue.push(node));
+    };
+
+    return false;
+  };
 }
 
 const graph = new Graph();
@@ -62,3 +80,7 @@ graph.print();
 console.log(graph.hasPathDfs('c', 'f')); // false
 console.log(graph.hasPathDfs('c', 'f')); // false
 console.log(graph.hasPathDfs('c', 'e')); // true
+
+console.log(graph.hasPathBfs('c', 'f')); // false
+console.log(graph.hasPathBfs('c', 'f')); // false
+console.log(graph.hasPathBfs('c', 'e')); // true
